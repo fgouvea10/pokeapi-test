b03bfea4d730253e64d32b6cc07f72d8069cf70a
fgouvea10 <felipegalvesouvea@gmail.com>
2022-10-26 18:37:19 -0300

    feat: add pokemon icon in header

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,17 +1,17 @@
-import { Heart } from 'phosphor-react';
 import { Link } from 'react-router-dom';
 
 import logoImg from '~/assets/logo.png';
+import pokemonIconImg from '~/assets/pokemon-icon.png';
 
 export function Header() {
   return (
-    <header className="w-full bg-yellow-500">
+    <header className="w-full bg-blue-800">
       <div className="w-full max-w-6xl my-0 mx-auto py-4 px-4 flex items-center justify-between">
         <Link to="/">
           <img src={logoImg} alt="" className="w-32 h-auto" />
         </Link>
         <a href="/favorites" title="Favorites" aria-label="Favorites pokemons">
-          <Heart size={24} className="text-blue-700" />
+          <img src={pokemonIconImg} alt="" className="w-8 h-auto" />
         </a>
       </div>
     </header>
