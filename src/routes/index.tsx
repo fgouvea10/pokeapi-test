f42acf03518d416be6d9fe3b6c32aebcb2a8de3a
fgouvea10 <felipegalvesouvea@gmail.com>
2022-10-26 17:48:14 -0300

    fix: routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,13 +4,20 @@ import {
   BrowserRouter,
 } from 'react-router-dom';
 
-import { AppLayout } from '~/layouts/app-layout';
+import { Header } from '~/components/layout/header';
+
+import { PokemonDetails } from '~/pages/details';
+import { Favorites } from '~/pages/favorites';
+import { Home } from '~/pages/home';
 
 export function Routes() {
   return (
     <BrowserRouter>
+      <Header />
       <ReactRouterRoutes>
-        <Route index element={<AppLayout />} />
+        <Route index element={<Home />} />
+        <Route path="/:id" element={<PokemonDetails />} />
+        <Route path="/favorites" element={<Favorites />} />
       </ReactRouterRoutes>
     </BrowserRouter>
   );
