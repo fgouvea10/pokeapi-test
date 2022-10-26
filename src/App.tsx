c18cc61abfdece9352d3c1264241e79f5d0026ff
fgouvea10 <felipegalvesouvea@gmail.com>
2022-10-26 17:48:36 -0300

    feat: create an empty array of pokemons in storage

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
+import { useEffect } from 'react';
+
 import { Routes } from './routes';
+
+import { getStorage, setStorage } from './services/storage';
+import storageKeys from './services/storage/keys';
 
 import './styles/global.css';
 
 export function App() {
+  // create array of pokemons if does not exists one in storage
+  useEffect(() => {
+    const pokemonsInStorage = getStorage(storageKeys.favoritesPokemons);
+    if (pokemonsInStorage === null) {
+      setStorage(storageKeys.favoritesPokemons, JSON.stringify([]));
+      return;
+    }
+  }, []);
+
   return <Routes />;
 }
