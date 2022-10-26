a017f454cb2de40f776cc9b6948e3a076a06c078
fgouvea10 <felipegalvesouvea@gmail.com>
2022-10-26 17:43:19 -0300

    feat: add logo as link

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,4 +1,5 @@
 import { Heart } from 'phosphor-react';
+import { Link } from 'react-router-dom';
 
 import logoImg from '~/assets/logo.png';
 
@@ -6,9 +7,11 @@ export function Header() {
   return (
     <header className="w-full bg-yellow-500">
       <div className="w-full max-w-6xl my-0 mx-auto py-4 px-4 flex items-center justify-between">
-        <img src={logoImg} alt="" className="w-32 h-auto" />
+        <Link to="/">
+          <img src={logoImg} alt="" className="w-32 h-auto" />
+        </Link>
         <a href="/favorites" title="Favorites" aria-label="Favorites pokemons">
-          <Heart size={24} className="text-black" />
+          <Heart size={24} className="text-blue-700" />
         </a>
       </div>
     </header>
