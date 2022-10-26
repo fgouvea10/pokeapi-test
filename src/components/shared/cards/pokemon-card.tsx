05b525593139a88c191355b393206fbd2388eb94
fgouvea10 <felipegalvesouvea@gmail.com>
2022-10-26 18:56:36 -0300

    refactor: add blue border when pokemon is favorite

--- src/components/shared/cards/pokemon-card.tsx
+++ src/components/shared/cards/pokemon-card.tsx
@@ -1,22 +1,20 @@
 import { Link } from 'react-router-dom';
 import { useCallback, useEffect, useState } from 'react';
 
-import { getPokemonData } from '~/services/useCases/get-pokemon-details';
+import { getPokemonData } from '~/services/use-cases/get-pokemon-details';
+import { PokemonType } from '~/domain/shared/pokemon';
 
-interface PokemonData {
-  img: string;
-  name: string;
-  id: string;
-}
+type PokemonData = Pick<PokemonType, 'img' | 'name' | 'id'>;
 
 interface PokemonCardProps {
   pokemon: {
     name: string;
     url: string;
   };
+  isFavorite: boolean;
 }
 
-export function PokemonCard({ pokemon }: PokemonCardProps) {
+export function PokemonCard({ pokemon, isFavorite }: PokemonCardProps) {
   const [pokemonData, setPokemonData] = useState<PokemonData>();
 
   const { name, url } = pokemon;
@@ -38,7 +36,9 @@ export function PokemonCard({ pokemon }: PokemonCardProps) {
   return (
     <Link
       to={`/${pokemonData?.id}`}
-      className="py-8 px-6 bg-stone-100 border border-stone-200 rounded flex flex-col items-center justify-center gap-4 text-xl text-stone-700"
+      className={`py-8 px-6 bg-stone-100 border border-stone-200 rounded-xl flex flex-col items-center justify-center gap-4 text-xl text-stone-700 ${
+        isFavorite ? 'border border-blue-700' : ''
+      }`}
     >
       <img src={pokemonData?.img} alt="" className="w-44" />
       {pokemon && name?.charAt(0).toUpperCase() + name?.slice(1)}
